8f6870f205a87e404fe7486563b9d647b37f9f65
Kainat Naz <49521321+Kainatn@users.noreply.github.com>
2019-08-25 21:29:44 +0500

    Done WhatsApp

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
 axios.get('https://tk-whatsapp.herokuapp.com/messages')
-.then(function (response) {
-  // handle success
-  console.log("sUCESS");
-  console.log(response);
-  for (let index = 0; index < response.data.length; index++) {
+  .then(function (response) {
+    // handle success
+    console.log("sUCESS");
+    console.log(response);
+    for (let index = 0; index < response.data.length; index++) {
       //const element = response.data[i];
-    let message = response.data[index];
-    
+      let message = response.data[index];
+
       document.querySelector("div").innerHTML += `
      <div id="img_div"> 
      <img src="${message.profilePic}"> <h2>${message.name}</h2> 
      </div>
      <div class="content"> 
     
-     <p class="time-left">${message.time}</p>
+     <p class="time-left">${message.time.slice(0, 8)}</p>
        <span>${message.numbUnread}</span>
       <p id="firstLine">${message.firstLine}</p>  
     </div>
       `
-  }
-})
-.catch(function (error) {
-  // handle error
-  console.log(error);
-})
-.finally(function () {
-  // always executed
-  console.log("fINAL");
-});
+    }
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+    console.log("fINAL");
+  });
 
 
